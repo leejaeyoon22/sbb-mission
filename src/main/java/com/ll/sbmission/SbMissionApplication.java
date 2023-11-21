78a4cab6e7ae6dcc4b0fa47421fef70b844e7335
leejaeyoon22 <tksska1514@gmail.com>
2023-11-21 09:30:15 +0900

    start

---
+++ src/main/java/com/ll/sbmission/SbMissionApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbmission/SbMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
