e93d3c196730d1703c0dd07329c86915d4a2d69b
leejaeyoon22 <tksska1514@gmail.com>
2023-11-24 09:50:19 +0900

    2-08 완료

--- src/main/java/com/ll/sbmission/MainController.java
+++ src/main/java/com/ll/sbmission/MainController.java
@@ -1,6 +1,7 @@
 package com.ll.sbmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,10 @@ public class MainController {
     @ResponseBody
     public String index() {
         return "안녕하세요 sbb에 오신 것을 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
